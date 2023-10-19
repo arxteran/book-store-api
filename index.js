4b9f47bb09ade8080568ecc968b2dd1f1157a97a
arxteran <trd.teranova@gmail.com>
2023-10-19 10:31:46 +0700

    apdet

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ const express = require('express')
 const app = express()
 const port = 2805
 
-app.get('/',(req,res) =>{
-    res.send("Hello ini express")
-})
+    // representasi data dari database
+    const data_users = [
+        { id: 1, name: "Ardithya Teranova", alamat: "Sukabumi" },
+        { id: 2, name: "Gita Mey Sita", alamat: "Bandung" },
+        { id: 3, name: "Savin Dwi Fayza", alamat: "Bandung" },
+        { id: 4, name: "Iurie MOther F", alamat: "Bandung" },
+    ];
 
-app.get('/awawawaw',(req,res) =>{
-    res.send("Hello ini awawawaw")
-})
+    app.get("/aw/:id", (req, res) => {
+        // get data dari parameter
+        let id = parseInt(req.params.id);
 
+        // get data dari database
+        let result;
+        const user = data_users.find((user) => user.id === id);
+        if (user) {
+            result = {
+            status: 200,
+            data: user,
+            };
+        }
+        res.json(result);
+    });
 app.listen(port,() => console.log(`Server running on port ${port}`))
